3bc8d3ecc7bd8a3f3f1427d981ab07eefd74081c
mmastin <matthew.mastin@gmail.com>
2020-03-02 17:27:26 -0500

    initial miner

---
+++ mine.py
@@ -0,0 +1,26 @@
+import hashlib
+import requests
+import sys
+import random
+
+def proof_of_work(last_proof, difficulty):
+    proof = 0
+    tries = 0
+    while valid_proof(last_proof, proof, difficulty) is False:
+        proof += random.randint(1, 17)
+        tries += 1
+    print(f'Proof found after {tries} attempts')
+    return proof
+
+
+def valid_proof(last_proof, proof, difficulty):
+    # hash(last_proof, proof) must contain N leading zeroes,
+    # where N is the current difficulty level
+
+    guess = f'{last_proof}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    if guess_hash[:difficulty] == '0' * difficulty:
+        print(f'Valid solution: {guess_hash} from proof {proof}')
+
+    return guess_hash[:difficulty] == '0' * difficulty
