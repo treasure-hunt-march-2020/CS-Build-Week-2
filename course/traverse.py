c8aeca74ac982ffa83b5625715d37e48c0372abd
Heorhii Siburov <g.siburov@gmail.com>
2020-03-02 16:45:37 -0800

    still working on traverse

--- course/traverse.py
+++ course/traverse.py
@@ -10,16 +10,44 @@ def init():
     print(res.json()) 
 
 def move(direction):
-    data = '{"direction":"s"}'
+    data = '{"direction":f{direction}'
     res = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers=headers, data=data)
-    print(res.json()) 
+    print(res.json())
+    return res.json()
 
-def traverse(self):
+def traverse(start_room):
     graph = Graph()
+    # Place for path
     stack = Stack()
-    option = Stack()
-    visited = set()
     path = []
+    # Place for room_id with non-visited directions
+    options = Stack()
+    # Place for visited rooms
+    visited = set()
+    # Pushing first room in stack
+    stack.push(start_room["room_id"])
+    graph.add_vertex(start_room["room_id"], start_room["title"], start_room["description"], start_room["coordinates"], start_room["exits"], start_room["cooldown"], start_room["errors"], start_room["messages"])
 
+    while stack.size() > 0:
+        room_id = stack.pop()
+        print("room_id: ", room_id)
+
+        if room_id not in visited:
+            visited.add(room_id)
+            path.append(room_id)
+        if len(visited) == 500:
+            return path
+        exits = graph.directions[room_id]
+        print("exits: ", exits)
+
+        directions = 0
+        for way in exits:
+            print("Way in exits: ", way, exits[way])
+            if exits[way] == "?":
+                directions +=1
+                print("way: ", way)
+                next_room = move(way)
+                print("next_room: ", next_room)
+                graph.add_vertex(next_room["room_id"], next_room["title"], next_room["description"], next_room["coordinates"], next_room["exits"], next_room["cooldown"], next_room["errors"], next_room["messages"])
 
 print(init())
